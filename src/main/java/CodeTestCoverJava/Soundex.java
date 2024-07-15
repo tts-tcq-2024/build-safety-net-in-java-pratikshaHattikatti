0fe786a0b0bd996f230d492f545177f9af9eea07
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-07-16 00:06:10 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,72 +1,61 @@
 package CodeTestCoverJava;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Soundex {
 
-	 private static final Map<Character , Character> soundexMapping = initSoundexMapping();
+    private static final Map<Character, Character> soundexMapping = initSoundexMapping();
 
-	    private static Map<Character , Character> initSoundexMapping() {
-	        Map<Character, Character> soundexMap = new HashMap<>();
-		    soundexMap.put('A','0');
-		    soundexMap.put('E','0');
-		    soundexMap.put('I','0');
-		    soundexMap.put('O','0');
-		    soundexMap.put('U','0');
-		    soundexMap.put('Y','0');
-		    soundexMap.put('H','0');
-		    soundexMap.put('W','0');
-		    soundexMap.put('B','1');
-		    soundexMap.put('F','1');
-		    soundexMap.put('P','1');
-		    soundexMap.put('V','1');
-		    soundexMap.put('C','2');
-		    soundexMap.put('G','2');
-		    soundexMap.put('J','2');
-		    soundexMap.put('Q','2');
-		    soundexMap.put('K','2');
-		    soundexMap.put('S','2');
-		    soundexMap.put('X','2');
-		    soundexMap.put('Z','2');
-		    soundexMap.put('D','3');
-		    soundexMap.put('T','3');
-		    soundexMap.put('L','4');
-		    soundexMap.put('M','5');
-		    soundexMap.put('N','5');
-		    soundexMap.put('R','6');
-	           return soundexMap;
-	    };
+    private static Map<Character, Character> initSoundexMapping() {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('B', '1');
+        map.put('F', '1');
+        map.put('P', '1');
+        map.put('V', '1');
+        map.put('C', '2');
+        map.put('G', '2');
+        map.put('J', '2');
+        map.put('K', '2');
+        map.put('Q', '2');
+        map.put('S', '2');
+        map.put('X', '2');
+        map.put('Z', '2');
+        map.put('D', '3');
+        map.put('T', '3');
+        map.put('L', '4');
+        map.put('M', '5');
+        map.put('N', '5');
+        map.put('R', '6');
+        return map;
+    }
 
-	    public static String generateSoundex(String name) {
-	        if (name == null || name.isEmpty()) {
-	            return "";
-	        }
+    public static String generateSoundex(String name) {
+        if (name == null || name.isEmpty()) {
+            return "";
+        }
 
-	        StringBuilder soundex = new StringBuilder();
-	        soundex.append(Character.toUpperCase(name.charAt(0)));
-	        char prevCode = getSoundexCode(name.charAt(0));
+        StringBuilder soundex = new StringBuilder();
+        soundex.append(Character.toUpperCase(name.charAt(0)));
+        char prevCode = getSoundexCode(name.charAt(0));
 
-	        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
-	            char code = getSoundexCode(name.charAt(i));
-	            if (code != '0' && code != prevCode) {
-	                soundex.append(code);
-	                prevCode = code;
-	            }
-	        }
+        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
+            char code = getSoundexCode(name.charAt(i));
+            if (code != '0' && code != prevCode) {
+                soundex.append(code);
+                prevCode = code;
+            }
+        }
 
-	        while (soundex.length() < 4) {
-	            soundex.append('0');
-	        }
+        while (soundex.length() < 4) {
+            soundex.append('0');
+        }
 
-	        return soundex.toString();
-	    }
+        return soundex.toString();
+    }
 
-	    private static char getSoundexCode(char c) {
-	        c = Character.toUpperCase(c);
-	        return soundexMapping.getOrDefault(c, '0');
-	    }
-	}
+    private static char getSoundexCode(char c) {
+        c = Character.toUpperCase(c);
+        return soundexMapping.getOrDefault(c, '0');
+    }
+}
