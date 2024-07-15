b213d47a16dac269c3f862916ceaa5699e814b0e
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-07-16 00:37:25 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,61 +1,79 @@
 package CodeTestCoverJava;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Soundex {
 
-    private static final Map<Character, Character> soundexMapping = initSoundexMapping();
+    	public static String generateSoundex(String name) {
+		if (isEmptyString(name)) {
+			return "";
+		}
+		StringBuilder soundex = buildSoundex(name);
+		while (soundex.length() < 4) {
+			soundex.append('0');
+		}
+		return soundex.toString();
+	}
 
-    private static Map<Character, Character> initSoundexMapping() {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('B', '1');
-        map.put('F', '1');
-        map.put('P', '1');
-        map.put('V', '1');
-        map.put('C', '2');
-        map.put('G', '2');
-        map.put('J', '2');
-        map.put('K', '2');
-        map.put('Q', '2');
-        map.put('S', '2');
-        map.put('X', '2');
-        map.put('Z', '2');
-        map.put('D', '3');
-        map.put('T', '3');
-        map.put('L', '4');
-        map.put('M', '5');
-        map.put('N', '5');
-        map.put('R', '6');
-        return map;
-    }
+	public static StringBuilder buildSoundex(String name) {
+		StringBuilder soundex = new StringBuilder();
+		soundex.append(Character.toUpperCase(name.charAt(0)));
+		char prevCode = getSoundexCode(name.charAt(0));
+		for (int index = 1; checkLength(index, name.length(), soundex.length()); index++) {
+			char code = getSoundexCode(name.charAt(index));
+			if (doAppend(code, prevCode)) {
+				soundex.append(code);
+				prevCode = code;
+			}
+		}
+		return soundex;
+	}
 
-    public static String generateSoundex(String name) {
-        if (name == null || name.isEmpty()) {
-            return "";
-        }
+	public static boolean checkLength(int index, int nameLength, int soundexLength) {
+		return index < nameLength && soundexLength < 4;
+	}
 
-        StringBuilder soundex = new StringBuilder();
-        soundex.append(Character.toUpperCase(name.charAt(0)));
-        char prevCode = getSoundexCode(name.charAt(0));
+	public static boolean doAppend(char code, char prevCode) {
+		return code != '0' && code != prevCode;
+	}
 
-        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
-            char code = getSoundexCode(name.charAt(i));
-            if (code != '0' && code != prevCode) {
-                soundex.append(code);
-                prevCode = code;
-            }
-        }
+	public static char getSoundexCode(char c) {
+		Map<Character, Character> characterMap = buildSoundexMap();
+		if(characterMap.containsKey(Character.toUpperCase(c))) {
+			return characterMap.get(Character.toUpperCase(c));
+		}
+		return '0';
+	}
 
-        while (soundex.length() < 4) {
-            soundex.append('0');
-        }
+	public static Map<Character, Character> buildSoundexMap() {
+		Map<Character, Character> characterMap = new HashMap<>();
+		characterMap.putAll(populateSoundexMap(Arrays.asList('B', 'F', 'P', 'V'), '1'));
+		characterMap.putAll(populateSoundexMap(Arrays.asList('C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z'), '2'));
+		characterMap.putAll(populateSoundexMap(Arrays.asList('D', 'T'), '3'));
+		characterMap.putAll(populateSoundexMap(Arrays.asList('L'), '4'));
+		characterMap.putAll(populateSoundexMap(Arrays.asList('M', 'N'), '5'));
+		characterMap.putAll(populateSoundexMap(Arrays.asList('R'), '6'));
+		return characterMap;
+	}
 
-        return soundex.toString();
-    }
+	public static Map<Character, Character> populateSoundexMap(List<Character> nameCharList, char code) {
+		Map<Character, Character> characterMap = new HashMap<>();
+		if(isEmptyList(nameCharList)) {
+			for(Character nameChar: nameCharList) {
+				characterMap.put(nameChar, code);
+			}
+		}
+		return characterMap;
+	}
 
-    private static char getSoundexCode(char c) {
-        c = Character.toUpperCase(c);
-        return soundexMapping.getOrDefault(c, '0');
-    }
+	public static boolean isEmptyString(String input) {
+		return input == null || input.isEmpty();
+	}
+
+	public static boolean isEmptyList(List<Character> inputList) {
+		return inputList != null && !inputList.isEmpty();
+	}
 }
